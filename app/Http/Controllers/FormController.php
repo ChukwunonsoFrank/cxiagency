d7afd9c003cb6c38ad09dd20a15c06d9d6160570
Chukwunonso Frank <franknonso114@gmail.com>
2025-08-25 22:14:33 +0100

    wip

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,21 +11,21 @@ class FormController extends Controller
 {
     public function submit(Request $request)
     {
-        // $recaptcha = $request->input('g-recaptcha-response');
+        $recaptcha = $request->input('g-recaptcha-response');
 
-        // if (is_null($recaptcha)) {
-        //     $request->session()->flash('message', "Please confirm you are not a robot.");
-        //     return redirect()->back();
-        // }
+        if (is_null($recaptcha)) {
+            $request->session()->flash('message', "Please confirm you are not a robot.");
+            return redirect()->back();
+        }
 
-        // $response = Http::get("https://www.google.com/recaptcha/api/siteverify", [
-        //     'secret' => config('services.recaptcha.secret'),
-        //     'response' => $recaptcha
-        // ]);
+        $response = Http::get("https://www.google.com/recaptcha/api/siteverify", [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $recaptcha
+        ]);
 
-        // $result = $response->json();
+        $result = $response->json();
 
-        // if ($response->successful() && $result['success'] == true) {
+        if ($response->successful() && $result['success'] == true) {
             $firstName = $request->input('first_name');
             $lastName = $request->input('last_name');
             $emailAddress = $request->input('email_address');
@@ -37,15 +37,15 @@ class FormController extends Controller
             $scamDate = $request->input('scam_date');
             $moreInfo = $request->input('more_info');
 
-            Notification::route('mail', 'info@cxiagency.com')->notify(new SendFormEntry($firstName, $lastName, $emailAddress, $country, $whatsappNumber, $telegramId, $whatHappened, $amountLost, $scamDate, $moreInfo));
+            Notification::route('mail', 'contact@cxiagency.com')->notify(new SendFormEntry($firstName, $lastName, $emailAddress, $country, $whatsappNumber, $telegramId, $whatHappened, $amountLost, $scamDate, $moreInfo));
 
             // return back()->with('message', 'Your submission has been recorded successfully.');
             $request->session()->flash('message', "Your submission has been recorded successfully.");
             return redirect()->back();
-        // } else {
-        //     // return back()->with('message', 'Please confirm you are not a robot.');
-        //     $request->session()->flash('message', "Please confirm you are not a robot.");
-        //     return redirect()->back();
-        // }
+        } else {
+            // return back()->with('message', 'Please confirm you are not a robot.');
+            $request->session()->flash('message', "Please confirm you are not a robot.");
+            return redirect()->back();
+        }
     }
 }
