5f9ae9a6932e2444c7240d01a67c26821d348d83
Chukwunonso Frank <franknonso114@gmail.com>
2025-09-02 20:26:18 +0100

    wip

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -4064,7 +4064,7 @@
                 })()
         })();
     </script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="referrer" content="strict-origin-when-cross-origin">
     <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
     <style>
